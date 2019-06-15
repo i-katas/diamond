20cac3ebdcdbcc38491f60d0bce6d7fb941df88c
selonj <selonj@163.com>
2019-06-15 22:07:47 +0800

    REFACTOR: use obvious implementation

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -3,11 +3,17 @@
  * @since 2019-06-15
  */
 public class Diamond {
+    private char letter;
+
+    public Diamond(char letter) {
+        this.letter = letter;
+    }
+
     public static Diamond of(char letter) {
-        return new Diamond();
+        return new Diamond(letter);
     }
 
     public String print() {
-        return "A";
+        return String.valueOf(letter);
     }
 }

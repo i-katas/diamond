80f4b65714a96514f90230fef4b30af5d8928734
selonj <selonj@163.com>
2019-06-15 22:32:35 +0800

    GREEN:make the test pass by change the condition of the if statement

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -3,6 +3,7 @@
  * @since 2019-06-15
  */
 public class Diamond {
+    private static final char TOP_LEVEL_CHAR = 'A';
     private char letter;
 
     public Diamond(char letter) {
@@ -16,8 +17,7 @@ public class Diamond {
     public String print() {
         StringBuilder out = new StringBuilder();
         out.append(letter);
-
-        if (letter == 'B') {
+        if (letter != TOP_LEVEL_CHAR) {
             out.append(letter);
         }
         return out.toString();
